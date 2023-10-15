9772d69eda4f2fa2f0f86dd40951344e7d4a7b11
ShoutingEagle <sameerpaul58@gmail.com>
2023-10-15 08:20:41 +0530

    commit second

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import {ReactComponent as Keyboard} from '../Assets/keyboard.svg'
+import {ReactComponent as User} from '../Assets/user.svg'
+
+
 function Navbar() {
   return (
-    <div>
-        <div>
-            <span>TypeCat</span>
-
+    <div className='navbar'>
+        <div className='navbar-right'>
+            <span className='logo'>TypeCat</span>
+            <Keyboard/>
         </div>
+        <div className='navbar-left'>
+            <User/>
+        </div>
+
     </div>
   )
 }
